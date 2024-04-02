c04acbe6c78ed00a48df45368cec841b0cc6f452
radianwir <wrradian@gmail.com>
2024-04-01 22:37:54 -0400

    First Github Project

---
+++ app.py
@@ -0,0 +1,12 @@
+print("## PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA")
+print("==========================================")
+print()
+
+def hitungluassegitiga(a,t):
+    return round(0.5 a * t, 2)
+
+alas = float (input ("input alas segitiga"))
+tinggi = float (input ("input tinggi segitiga"))
+
+print('luas segitiga = ', hitungLuasSegitiga(alas,tinggi))
+print()
