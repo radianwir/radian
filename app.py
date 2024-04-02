00d0378ed3de6dcb896cab7af391695ffa430363
radianwir <wrradian@gmail.com>
2024-04-01 23:07:45 -0400

    ini commit kedua

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ tinggi = float (input ("input tinggi segitiga"))
 
 print('luas segitiga = ', hitungLuasSegitiga(alas,tinggi))
 print()
+
+print("## PROGRAM PYTHON MENGHITUNG LUAS SEGITIGA")
+print("==========================================")
+print()
+
+def hitungpersegipanjang(p,l):
+    return round(p * 1,2)
+
+panjang = float(input('input panjang : '))
+lebar = float(input('inputlebar :'))
+print('luas persegi panjang = ',hitungpersegipanjang(panjang,lebar))
